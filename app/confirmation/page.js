071b497b765cc157f40833a1c0bc39f5394e3091
LeanneCodes <leannegoldsmith95@yahoo.com>
2025-11-13 17:27:06 +0000

    add necessary starter pages

---
+++ app/confirmation/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      confirmation page
+    </div>
+  )
+}
+
+export default page
---
+++ app/products/[id]/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      product id page
+    </div>
+  )
+}
+
+export default page
